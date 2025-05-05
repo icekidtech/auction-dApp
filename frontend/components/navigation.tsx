bbc7cc3f5c5608f5c33d6905906ab67e05d1813f
icekidtech <princekelvin7568@gmail.com>
2025-05-05 14:33:29 +0100

    refactor: remove unused Button import from Navigation component

--- frontend/components/navigation.tsx
+++ frontend/components/navigation.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { WalletConnect } from "@/components/wallet-connect";
 import { useWallet } from "@/hooks/use-wallet";
 
